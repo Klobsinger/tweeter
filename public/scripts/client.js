60f22228cfdcd311eafaf80b379edde41f596240
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-07-15 17:12:19 -0400

    Feature: add escape function to prevent script injection

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 // This script handles form submission, tweet creation, rendering tweets, and loading tweets from the server.
 // It utilizes jQuery and timeago.js for various functionalities.
 
@@ -42,29 +46,29 @@ $(document).ready(function() {
  */
   const createTweetElement = function(tweet) {
     const $tweet = $(`
-  <article class="tweet">
-    <header>
-      <div class="left-section">
-        <img src="${tweet.user.avatars}" alt="Profile Image">
-        <p class="name">${tweet.user.name}</p>
-      </div>
-      <p class="handle">${tweet.user.handle}</p>
-    </header>
-    <div class="tweet">
-      <p class="text">${tweet.content.text}</p>
-    </div>
-    <footer> 
-      <div class="left">
-        <span class="created-at">${tweet.created_at}</span>
-      </div>
-      <div class="symbol">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-sharp fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>
-`);
+      <article class="tweet">
+        <header>
+          <div class="left-section">
+            <img src="${escape(tweet.user.avatars)}" alt="Profile Image">
+            <p class="name">${escape(tweet.user.name)}</p>
+          </div>
+          <p class="handle">${escape(tweet.user.handle)}</p>
+        </header>
+        <div class="tweet">
+          <p class="text">${escape(tweet.content.text)}</p>
+        </div>
+        <footer> 
+          <div class="left">
+            <span class="created-at">${escape(tweet.created_at)}</span>
+          </div>
+          <div class="symbol">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-sharp fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
     return $tweet;
   };
 /**
